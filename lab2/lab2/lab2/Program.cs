4119cd0064db24d4b4bf94e845f7cfcad752a2f8
nishi1999 <73095402+nishi1999@users.noreply.github.com>
2020-10-19 20:02:21 +0600

    Initial change

---
+++ lab2/lab2/lab2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* int i;
+             int n = 100;
+             for (i = 1; i <= n; i++)
+             {
+                 if (i % 2 == 0)
+                 {
+                     Console.WriteLine("Result =" + i);
+                 }
+             }*/
+            /*int x = 0;
+            for (int i = 0; i< 4; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    x = i + j;
+                    if (x % 2 == 0)
+                    {
+                        Console.Write("X");
+
+                    }
+                    else
+                    {
+                        Console.Write("0");
+
+                    }
+
+                }
+                Console.WriteLine();
+            }*/
+
+            /* int y = 0;
+             for (int i = 1; i <= 6; i++)
+             {
+                 y = i;
+                 for (int j = 0; j <= 3; j++)
+                 {
+                     Console.Write(y + j);
+                 }
+                 Console.WriteLine();
+             }
+             Console.ReadLine();
+         }*/
+            /*{
+                    int i;
+                    int num = 100;
+                    int oddSum = 0;
+                    int evenSum = 0;
+
+                    for (i = 1; i <= num; i++)
+                    {
+                        if (i % 2 == 0)
+                            evenSum = evenSum + i;
+                        else
+                            oddSum = oddSum + i;
+                    }
+                    Console.WriteLine("sum of odd " + oddSum);
+                    Console.WriteLine("sum of even" + evenSum);
+                }*/
+            /* int i, j;
+
+             for (i = 1; i <= 5; i++)
+             {
+
+                 for (j = 1; j <= 5; j++)
+                 {
+                     if (i == j)
+                     {
+                         Console.Write(j);
+                     }
+                     else
+
+                         Console.Write(" ");
+                 }
+                 Console.WriteLine();
+
+             }
+     }*/
+            
+            /*int i, f = 1;
+            Console.WriteLine("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+
+            {
+                f = f * i;
+                Console.WriteLine(f);
+            }*/
+
+           
+            /*  int n = 6;
+                    for (int i = 1; i < n; i++)
+                    {
+                        for (int j = 1; j <=i; j++)
+                        {
+                            Console.Write("*");
+
+                        }
+                        Console.WriteLine();
+
+                    }
+
+
+                    for (int i = 1; i < n; i++)
+                    {
+                        for (int j = n-2; j >= i; j--)
+                        {
+                            Console.Write("*");
+
+                        }
+                        Console.WriteLine();
+
+
+
+                    }
+
+                    Console.WriteLine();
+                    }*/
+
+
+
+
+        }
+    }
+}
+
+
+
+
