53a9c7b1f6b38a793c2c74b6c772859551ca511b
unknown <you@example.com>
2020-10-19 14:24:17 +0600

    first commit

---
+++ lab/lab/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lab
+{
+    class Program
+    {
+       
+      
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine("Nishi");
+                Console.WriteLine("");
+                int x, y, z;
+                x = 5;
+                y = 10;
+                z = x + y; a
+                Console.WriteLine("x+y= " + z);
+                Console.WriteLine("");
+                int m = 5;
+                int n = 20;
+                Console.WriteLine("m/n= " + (m / n));
+                Console.WriteLine("");
+                int a, b, c, d;
+                a = -5 + 8 * 6;
+                b = (55 + 9) % 9;
+                c = 20 + -3 * 5 / 8;
+                d = 5 + 15 / 3 * 2 - 8 % 3;
+                Console.WriteLine("a= " + a);
+                Console.WriteLine("b= " + b);
+                Console.WriteLine("c= " + c);
+                Console.WriteLine("d= " + d);
+                Console.WriteLine("");
+                int e, f;
+                e = 10;
+                f = 3;
+                Console.WriteLine("e+f= " + (e + f));
+                Console.WriteLine("e*f= " + (e * f));
+                Console.WriteLine("e-f= " + (e - f));
+                Console.WriteLine("e/f= " + (e / f));
+                Console.WriteLine("e%f= " + (e % f));
+                Console.WriteLine("");
+                double i;
+                i = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+                Console.WriteLine("ques 6= " + i);
+                Console.WriteLine("");
+                double Width = 5.5;
+                double Height = 8.5;
+                Console.WriteLine("Area is: " + (Height * Width));
+                Console.WriteLine("Perimeter is: " + 2 * (Height + Width));
+
+            }
+        }
+    }
+
+    
