4b32c8bce15476cbce6259dd71595635a7b6b457
nishi1999 <73095402+nishi1999@users.noreply.github.com>
2020-11-19 20:49:26 +0600

    lab task 6

--- Lab task/Lab task/Program.cs
+++ Lab task/Lab task/Program.cs
@@ -3,10 +3,46 @@
 namespace Lab_task
 {
     class Program
-    {
-        static void Main(string[] args)
+   
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                Student s = new Student("Jannatul", "19-40374-1", "CSE", 3.72f);
+
+                s.ShowInfo();
+                
+
+                Book b = new Book("sashar kobita", "Rrobindronath", "10", "upponnash", 200);
+                b.ShowInfo();
+                
+
+                
+
+                b.AddBookCopy(10);
+                b.ShowInfo();
+               
+
+                Account sa = new Account("jannatul", "19403741", 10000);
+                sa.ShowInfo();
+                
+
+                Library l = new Library("New library, "danmondi"," dhaka", 30000);
+                l.ShowLibInfo();
+               
+
+
+
+
+                l.AddNewBook(b);
+                
+           
+                l.ShowAllBooks();
+
+                l.AddNewBookCopy(b, 70);
+
+
+                l.ShowAllBooks();
+
+            }
         }
     }
-}
